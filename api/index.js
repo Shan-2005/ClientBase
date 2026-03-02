5791697038da3914e781975c67c3f9debdd7ae46
Shan-2005 <sn0760@srmist.edu.in>
2026-03-02 09:43:33 +0530

    chore: clean up debug code in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,6 @@
+const fastify = require('../server');
+
 module.exports = async (req, res) => {
-    try {
-        const fastify = require('../server');
-        await fastify.ready();
-        fastify.server.emit('request', req, res);
-    } catch (err) {
-        res.status(500).json({
-            error: err.message,
-            stack: err.stack,
-            env: process.env.NODE_ENV,
-            cwd: process.cwd(),
-            dataDir: process.env.DATA_DIR
-        });
-    }
+    await fastify.ready();
+    fastify.server.emit('request', req, res);
 };
