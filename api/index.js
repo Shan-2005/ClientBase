d2f62fc8ada67901f29daabbc8208a0ceea9d631
Shan-2005 <sn0760@srmist.edu.in>
2026-03-02 08:24:34 +0530

    debug: add error handling to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
-const fastify = require('../server');
-
 module.exports = async (req, res) => {
-    await fastify.ready();
-    fastify.server.emit('request', req, res);
+    try {
+        const fastify = require('../server');
+        await fastify.ready();
+        fastify.server.emit('request', req, res);
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+            stack: err.stack,
+            env: process.env.NODE_ENV,
+            cwd: process.cwd(),
+            dataDir: process.env.DATA_DIR
+        });
+    }
 };
